0c03765e778a9dfc82b073e22944379bf1bc62f5
Eric Liu <sqfkw1578@gmail.com>
2020-07-03 08:40:21 +0800

    setTodoListState 裡面的 state會比較舊

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,29 +1,45 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
+import _ from 'lodash';
 import InputAndSubmit from './InputAndSubmit';
 import TodoList from './TodoList';
 
 export default function TodoApp() {
   const [todoListState, setTodoListState] = useState([]);
+  const newTodoListState2 = todoListState;
   const [idClick, setIdClick] = useState(null);
   const changeTodoItemIsDone = (id: number) => {
-    const newTodoListState = JSON.parse(JSON.stringify(todoListState));
-    console.log(newTodoListState);
+    // const newTodoListState = JSON.parse(JSON.stringify(todoListState));
+    const newTodoListState = _.cloneDeep(newTodoListState2);
     if (newTodoListState[id]) newTodoListState[id].isDone = !newTodoListState[id].isDone;
-    console.log(newTodoListState);
+    // for (let i = 0; i < newTodoListState.length; i += 1) {
+    //   newTodoListState[id].onClick = todoListState[id].onClick;
+    // }
+    console.log("clicked");
+    // if (newTodoListState[id]) newTodoListState[id].isDone = !newTodoListState[id]?.isDone;
     setTodoListState(newTodoListState);
   };
-  useEffect(() => {
-    changeTodoItemIsDone(idClick);
-  }, [idClick]);
+
+  const onTodoItemClick = () => {//id: number
+    // console.log("onTodoItemClick");
+    // setIdClick(id);
+    // changeTodoItemIsDone(id);
+    // console.log(id);
+
+    console.log(newTodoListState2);
+    // const newTodoListState = _.cloneDeep(todoListState);
+    // console.log(newTodoListState);
+    // if (newTodoListState[id]) newTodoListState[id].isDone = !newTodoListState[id].isDone;
+    // setTodoListState(newTodoListState);
+  };
   const onSubmitTodo = (value: string) => {
     setTodoListState([...todoListState, {
       todo: value,
       isDone: false,
       id: todoListState.length,
       onClick: () => {
-        console.log("clicked");
-        setIdClick(todoListState.length);
+        // const idd = todoListState.length;
+        onTodoItemClick();
       },
     }]);
   };

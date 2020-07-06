addd3218edb92326e09a1ac9350a8ab0c9da6d63
Eric Liu <sqfkw1578@gmail.com>
2020-07-06 14:14:21 +0800

    新增內容

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ cd build
 git init
 git add .
 git commit -m "build"
-git remote add origin https://github.com/edinliu/ts-todolist.git
-git push -u origin master
+git remote add origin https://github.com/edinliu/ts-todolist-demo.git
+git push -f origin master
+cd ..
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # 簡介
 
 這是使用 typescript 製作的 Todo List
+
+# 連結
+
+- <a href='https://edinliu.github.io/ts-todolist-demo/'>demo</a>
+- <a>原始碼</a>

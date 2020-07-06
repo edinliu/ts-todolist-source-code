ed14bc2e3797212b25c576872a09d46e685ae3d2
Eric Liu <sqfkw1578@gmail.com>
2020-07-06 13:54:50 +0800

    修改內容

--- readme.md
+++ readme.md
@@ -1,13 +1,3 @@
 # 簡介
 
-typescript todo list
-
-# 遇到問題
-
-物件含有 function，經過 JSON.parse(JSON.stringify(obj)) 完全複製，function 會消失
-
-# 解決方法
-
-https://larry850806.github.io/2016/09/20/shallow-vs-deep-copy/
-
-使用 Object.assign
+這是使用 typescript 製作的 Todo List
--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -8,25 +8,17 @@ export default function TodoApp() {
   const [todoListState, setTodoListState] = useState([]);
   const [whichTodoItemClick, setWhichTodoItemClick] = useState(null);
   const [filter, setFilter] = useState('all');
-  // 步驟3. 使用 changeTodoIsDone 去 setTodoListState 並且把 setWhichTodoItemClick 設回預設值 null
   const changeTodoIsDone = (id: number) => {
-    const copy = Object.assign([], todoListState);
-    copy[id].isDone = !copy[id].isDone;
-    setTodoListState(copy);
+    todoListState[id].isDone = !todoListState[id].isDone;
+    setTodoListState(todoListState);
     setWhichTodoItemClick(null);
   };
-  // 步驟2. 使用 useEffect 去偵測 setWhichTodoItemClick 並執行 changeTodoIsDone
   useEffect(() => {
     if (whichTodoItemClick !== null) {
       changeTodoIsDone(whichTodoItemClick);
     }
   }, [whichTodoItemClick]);
-  /**
-    步驟1: 創建 onSubmitTodo 讓 <InputAndSubmit/> 使用 setTodoListState 更新 todoListState，並且製作一個閉包函式
-    去紀錄當時的 todoListState.length 並作為 <TodoItem/> 的 onClick，當onClick執行時會用 todoListState.length
-    去 setWhichTodoItemClick 改變本組件的 state
-  */
-  const onSubmitTodo = (value: string) => {
+  const handleSubmit = (value: string) => {
     const newTodoList = todoListState.concat([{
       todo: value,
       isDone: false,
@@ -39,7 +31,7 @@ export default function TodoApp() {
   };
   return (
     <div className="p-5">
-      <InputAndSubmit onSubmit={onSubmitTodo} />
+      <InputAndSubmit onSubmit={handleSubmit} />
       <TodoList todoList={todoListState} filter={filter} />
       <TodoFilterPannel onClick={setFilter} />
     </div>

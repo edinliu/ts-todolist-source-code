d908d58e5143a1dbb74cfb274797035227e398d9
Eric Liu <sqfkw1578@gmail.com>
2020-07-19 12:00:50 +0800

    改成async await

--- src/components/Data/functions/readCollection.ts
+++ src/components/Data/functions/readCollection.ts
@@ -1,22 +1,17 @@
 import * as firebase from 'firebase/app';
 
-export default function readCollection(
+export default async function readCollection(
   collectionName: string, setMessage?: (message: string) => void,
 ) {
   if (collectionName) {
     const db = firebase.firestore();
-    db.collection(collectionName).get()
-      .then((querySnapshot) => {
-        const idAndData = {};
-        querySnapshot.forEach((doc) => {
-          // console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-          idAndData[doc.id] = doc.data();
-        });
-        return idAndData;
-      })
-      .then((data) => {
-        setMessage(JSON.stringify(data));
-      });
+    const idAndData = {};
+    const querySnapshot = await db.collection(collectionName).get();
+    querySnapshot.forEach((doc) => {
+      // console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+      idAndData[doc.id] = doc.data();
+    });
+    setMessage(JSON.stringify(idAndData));
   } else {
     setMessage('missing collection name');
   }
